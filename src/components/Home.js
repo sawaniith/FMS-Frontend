7881eb71a8101b496a5ab715c31ac15efa32527c
Sawan Kumar <sawanjnv206@gmail.com>
2023-09-01 18:07:18 +0530

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -77,7 +77,9 @@ const Home = () => {
     }
   }
 
-  getFarmer()
+  useEffect(() => {
+    getFarmer();
+  }, []);
 
   const edit = (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y) => {
     navigate('/edit', {
